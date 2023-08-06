93a8204ae1abda4687d8d1c8219a61dd36d2fdcb
sjkingdev <helloseanking@yahoo.com>
2023-08-06 19:33:55 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("click", e => {
+    let handle
+    if(e.target.matches(".handle")) {
+        handle = e.target
+    } else {
+        handle = e.target.closest(".handle")
+    }
+
+    if (handle != null) {
+        onHandleClick(handle)
+    }
+})
+
+function onHandleClick(handle) {
+    // alert("click slider control")
+    const slider = handle.closest(".container").querySelector(".slider");
+
+    // const sliderIndex = parseInt(slider.style.getPropertyValue("--slider"))
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"));
+
+    if (handle.classList.contains("left-handle")) {
+        slider.style.setProperty("--slider-index", sliderIndex -1);
+    }
+
+    if (handle.classList.contains("right-handle")) {
+        slider.style.setProperty("--slider-index", sliderIndex +1);
+    }
+}
